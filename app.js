9d0b16a10a8a28ee5546039c56d155152b3b6201
Andrea Arroyo <103694235+Aarroyoco@users.noreply.github.com>
2025-03-18 00:37:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objtivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+function agregarAmigo(){
+    if (inputAmigo.value == ""){
+        alert("Debes ingresar un nombre")
+    }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+function sortearAmigo(){
+    if (listaAmigos.length === 0) {
+        alert("No hay amigos en la lista para sortear");
+        return;
+    }
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+}
